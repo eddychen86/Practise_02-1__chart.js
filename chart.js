e62c86a435ff827d8416d541d61db3b306126acc
eddychen86 <eddychen101020@gmail.com>
2022-02-23 10:20:39 +0800

    Step 2 : setting style

--- chart.js
+++ chart.js
@@ -1,1 +1,12 @@
-var ctx = 'myChart';
+var canvas = document.getElementById('myChart');
+if (canvas.getContext) {
+  var ctx = canvas.getContext('2d');
+
+  // fillStyle：填色
+  ctx.fillStyle = 'rgb(200,0,0)';
+  // fillRect：定義形狀
+  ctx.fillRect(10, 10, 55, 50);
+
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  ctx.fillRect(30, 30, 55, 50);
+}
